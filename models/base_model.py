a1b68291e044cbb11924359fa05ce8f3b755d692
yvonnegichovi <yvonnegichovi@gmail.com>
2024-01-09 12:19:03 -0800

    basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -2,8 +2,7 @@
 """This module has class BaseModel that defines all common attributes/ methods
 for other classes"""
 
-import models
-from uuid import uuid4
+import uuid
 from datetime import datetime
 
 
@@ -13,18 +12,18 @@ class BaseModel:
     def __init__(self):
         """Initializes all instances attributes used"""
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
 
     def __str__(self):
         """returns an informal string representation of the instance"""
-        print("[{}] ({}) {}".format(
+        return ("[{}] ({}) {}".format(
             self.__class__.__name__, self.id, self.__dict__))
 
     def save(self):
         """updates the public instance attribute updated_at with the current
         datetime"""
-        self.update_at = datetime.now()
+        self.updated_at = datetime.utcnow()
 
     def to_dict(self):
         """return a dictionary containing all keys/values of __dict__ of the
@@ -34,3 +33,17 @@ class BaseModel:
         obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.updated_at.isoformat()
         return obj_dict
+
+
+if __name__ == "__main__":
+    my_model = BaseModel()
+    my_model.name = "My First Model"
+    my_model.my_number = 89
+    print(my_model)
+    my_model.save()
+    print(my_model)
+    my_model_json = my_model.to_dict()
+    print(my_model_json)
+    print("JSON of my_model:")
+    for key in my_model_json.keys():
+        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
