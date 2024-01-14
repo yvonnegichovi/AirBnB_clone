1333619241b285ae187c0ba141de14586adeae44
yvonnegichovi <yvonnegichovi@gmail.com>
2024-01-14 02:57:19 -0800

    console tests

---
+++ tests/test_console.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+import unittest
+from unittest.mock import patch
+from io import StringIO
+from console import HBNBCommand
+
+
+class TestConsoleHelp(unittest.TestCase):
+    def setUp(self):
+        """sets up the console class before every test"""
+        self.console = HBNBCommand()
+
+    def tearDown(self):
+        """passes"""
+        pass
+
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_help_quit_command(self, mock_stdout):
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.console.onecmd("help quit")
+            expected_output = "" 
+            output = mock_stdout.getvalue().strip()
+            self.assertEqual(expected_output, output)
+
+
+if __name__ == "__main__":
+    unittest.main()
