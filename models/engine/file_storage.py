a8b64b5b332aa9f5fa7b6b8f5b6ef3042a7364c4
yvonnegichovi <yvonnegichovi@gmail.com>
2024-01-14 22:27:08 -0800

    dict

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -44,7 +44,7 @@ class FileStorage:
         }
         try:
             temp = {}
-            with open(FileStorage.__file_path, 'r') as f:
+            with open(FileStorage.__file_path, 'r', encoding='utf-8') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
                     self.all()[key] = classes[val['__class__']](**val)
